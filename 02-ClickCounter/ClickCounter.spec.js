aa90382da5e2e7504263c2ae72610cc6a714959c
김정환 <ej88ej@gmail.com>
2017-08-03 20:06:26 +0900

    ClickCounter spec작성

---
+++ 02-ClickCounter/ClickCounter.spec.js
@@ -0,0 +1,13 @@
+describe('ClickCounter', ()=> {
+  describe('getCounter()', ()=> {
+    it('초기값이 0인 카운터 값을 반환한다', ()=> {
+      // todo 
+    })
+  })
+
+  describe('increase()', ()=> {
+    it('카운터를 1 올린다', ()=> {
+      // todo 
+    })
+  })
+})
