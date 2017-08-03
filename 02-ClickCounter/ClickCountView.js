12b51ce21e68c1f96e7befa319ff9eb37d6f6946
김정환 <ej88ej@gmail.com>
2017-08-04 00:11:29 +0900

    네거티브 테스트 추가

--- 02-ClickCounter/ClickCountView.js
+++ 02-ClickCounter/ClickCountView.js
@@ -1,5 +1,6 @@
 var ClickCountView = function(clickCounter, updateEl) {
-
+  if (!clickCounter) throw new Error(ClickCountView.messages.noClickCounter)
+  if (!updateEl) throw new Error(ClickCountView.messages.noUpdateEl)
   return {
     updateView() {
       updateEl.text(clickCounter.getCounter())
@@ -10,4 +11,9 @@ var ClickCountView = function(clickCounter, updateEl) {
       this.updateView()
     }
   }
+}
+
+ClickCountView.messages = {
+  noClickCounter: 'clickCount를 주입해야 합니다',
+  noUpdateEl: 'updateEl를 주입해야 합니다'
 }
--- 02-ClickCounter/ClickCountView.spec.js
+++ 02-ClickCounter/ClickCountView.spec.js
@@ -1,11 +1,26 @@
 describe('ClickCountView 모듈', () => {
   let udpateEl, clickCounter, view
 
+  it('ClickCounter를 주입하지 않으면 에러를 던진다', ()=> {
+    const clickCounter = null
+    const updateEl = $('<span></span>') 
+    const actual = () => ClickCountView(clickCounter, updateEl)
+    expect(actual).toThrowError(ClickCountView.messages.noClickCounter)
+  })
+
+  it('updateEl를 주입하지 않으면 에러를 던진다', ()=> {
+    const clickCounter = ClickCounter()
+    const updateEl = null 
+    const actual = () => ClickCountView(clickCounter, updateEl)
+    expect(actual).toThrowError(ClickCountView.messages.noUpdateEl)
+  })
+
   beforeEach(()=> {
     updateEl = $('<span></span>');
     clickCounter = ClickCounter(); 
     view = ClickCountView(clickCounter, updateEl)
   })
+  
 
   describe('updateView()', () => {
     it('ClickCounter의 getCounter() 실행결과를 출력한다', ()=> {
