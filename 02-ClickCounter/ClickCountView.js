356442439e37f8f6b69ad0de274dd51495e3eaff
김정환 <ej88ej@gmail.com>
2017-08-04 00:11:29 +0900

    네거티브 테스트 추가

--- 02-ClickCounter/ClickCountView.js
+++ 02-ClickCounter/ClickCountView.js
@@ -1,8 +1,19 @@
 var ClickCountView = function(clickCounter, updateEl) {
-
+  if (!clickCounter) throw new Error(ClickCountView.messages.noClickCounter)
+  if (!updateEl) throw new Error(ClickCountView.messages.noUpdateEl)
   return {
     updateView() {
       updateEl.text(clickCounter.getCounter())
-    }
+    },
+
+    increaseAndUpdateView() {
+      clickCounter.increase()
+      this.updateView()
+    },
   }
+}
+
+ClickCountView.messages = {
+  noClickCounter: 'clickCount를 주입해야 합니다',
+  noUpdateEl: 'updateEl를 주입해야 합니다'
 }
