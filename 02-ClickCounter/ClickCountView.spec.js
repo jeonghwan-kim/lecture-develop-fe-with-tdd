0d6595616840519d3e20baf602ad206f1e953911
김정환 <ej88ej@gmail.com>
2017-08-09 20:08:02 +0900

    파래매터 tc를  네거티브 테스트로 분리

--- 02-ClickCounter/ClickCountView.spec.js
+++ 02-ClickCounter/ClickCountView.spec.js
@@ -1,26 +1,23 @@
 describe('ClickCountView 모듈', () => {
   let udpateEl, clickCounter, view
-
-  it('ClickCounter를 주입하지 않으면 에러를 던진다', ()=> {
-    const clickCounter = null
-    const updateEl = $('<span></span>') 
-    const actual = () => ClickCountView(clickCounter, updateEl)
-    expect(actual).toThrowError(ClickCountView.messages.noClickCounter)
-  })
-
-  it('updateEl를 주입하지 않으면 에러를 던진다', ()=> {
-    const clickCounter = ClickCounter()
-    const updateEl = null 
-    const actual = () => ClickCountView(clickCounter, updateEl)
-    expect(actual).toThrowError(ClickCountView.messages.noUpdateEl)
-  })
-
+  
   beforeEach(()=> {
     updateEl = $('<span></span>');
     clickCounter = ClickCounter(); 
     view = ClickCountView(clickCounter, updateEl)
   })
   
+  describe('네거티브 테스트', ()=> {
+    it('ClickCounter를 주입하지 않으면 에러를 던진다', ()=> {
+      const actual = () => ClickCountView(null, updateEl)
+      expect(actual).toThrowError(ClickCountView.messages.noClickCounter)
+    })
+
+    it('updateEl를 주입하지 않으면 에러를 던진다', ()=> {
+      const actual = () => ClickCountView(clickCounter, null)
+      expect(actual).toThrowError(ClickCountView.messages.noUpdateEl)
+    })
+  })
 
   describe('updateView()', () => {
     it('ClickCounter의 getCounter() 실행결과를 출력한다', ()=> {
