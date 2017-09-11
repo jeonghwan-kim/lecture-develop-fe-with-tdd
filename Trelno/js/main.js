51ef36c58a4d4c55b2a4f34a19daacb4030b9a66
김정환 <ej88ej@gmail.com>
2017-09-12 00:12:14 +0900

    SidebarView

--- Trelno/js/main.js
+++ Trelno/js/main.js
@@ -1,31 +1,12 @@
-function updateBoardDisplay() {
-  console.log('updateBoardDisplay')
-
-  const cardList = Trelno.CardService().query()
-  const board1CardList = cardList.filter(c => c.boardId === 1)
-  const board2CardList = cardList.filter(c => c.boardId === 2)
-
-  var board1 = Trelno.BoardDisplay(1, 'To Do', board1CardList.map(c => Trelno.CardDisplay(c)))
-  var board2 = Trelno.BoardDisplay(2, 'Doing', board2CardList.map(c => Trelno.CardDisplay(c)))
-
-  $('#board-list')
-    .empty()
-    .append(board1.element())
-    .append(board2.element())
-}
-
-(function() {
-  var onLoad = function () {
-    Trelno.SidebarDisplay($('#show-menu-link'), $('#sidebar'))
-    Trelno.BgColorSettingDisplay($('.palette'), $('body'))
-
-    updateBoardDisplay()
-
-    $('body').on('updateBoardDisplay', updateBoardDisplay)
-
-    // Trelno.CardService().create('card 1', 1)
-    // Trelno.CardService().create('card 2', 1)
-    // Trelno.CardService().create('card 3', 2)
+(() => {
+  const onLoad = () => {
+    Trelno.SidebarView({
+      el: '.sidebar',
+      data: {
+        bgColor: '#ff0000'
+      },
+      triggerEl: '#show-menu-link', 
+    });
   };
 
   document.addEventListener('DOMContentLoaded', onLoad);
