e51d10aa3b3c19e7790e9db16028f250ed761bee
김정환 <6pack@woowahan.com>
2018-04-14 20:59:01 +0900

    Counter spec4 테스트 코드 시작

--- ClickCounter/Counter.spec.js
+++ ClickCounter/Counter.spec.js
@@ -1,20 +1,28 @@
 describe('App.Counter', ()=> {
-  let counter
-
-  beforeEach(()=> {
-    counter = App.Counter();
-  })
-
-  describe('getValue()', ()=> {
-    it('초기값이 0인 값을 반환한다', ()=> {
-      expect(counter.getValue()).toBe(0)
+  describe('초기값을 주입하지 않으면', () => {
+    it('에러를 던진다', () => {
+      // todo 
     })
   })
 
-  describe('count()', ()=> {
-    it('값을 1 증가한다', ()=> {
-      counter.count();
-      expect(counter.getValue()).toBe(1)
+  describe('초기값을 주입하면', () => {
+    let counter
+
+    beforeEach(()=> {
+      counter = App.Counter();
+    })
+  
+    describe('getValue()', ()=> {
+      it('초기값이 0인 값을 반환한다', ()=> {
+        expect(counter.getValue()).toBe(0)
+      })
+    })
+  
+    describe('count()', ()=> {
+      it('값을 1 증가한다', ()=> {
+        counter.count();
+        expect(counter.getValue()).toBe(1)
+      })
     })
   })
 })
