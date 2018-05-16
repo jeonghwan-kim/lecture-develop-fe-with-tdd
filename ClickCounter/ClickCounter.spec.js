6005b7d08d785307d4b9a1e2a2034c5c10879c9f
김정환 <6pack@woowahan.com>
2018-05-16 22:09:18 +0900

    module

--- ClickCounter/ClickCounter.spec.js
+++ ClickCounter/ClickCounter.spec.js
@@ -1,14 +1,14 @@
-describe('ClickCounter', ()=> {
+describe('App.ClickCounter', ()=> {
   describe('getValue()', ()=> {
     it('초기값이 0인 카운터 값을 반환한다', ()=> {
-      const counter = ClickCounter()
+      const counter = App.ClickCounter()
       expect(counter.getValue()).toBe(0)
     })
   })
 
   describe('increase()', ()=> {
     it('카운터를 1 올린다', ()=> {
-      // todo 
+      todo 
     })
   })
 })
