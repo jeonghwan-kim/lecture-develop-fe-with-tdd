fde662007d3616df279853c72c983bf4de6969fa
김정환 <6pack@woowahan.com>
2018-05-17 23:49:06 +0900

    ClickCounter: 데이터 주입

--- ClickCounter/ClickCounter.js
+++ ClickCounter/ClickCounter.js
@@ -1,15 +1,17 @@
 var App = App || {}
 
-App.ClickCounter = () => {
-  let value = 0
+App.ClickCounter = _data => {
+  if (!_data) throw Error('_data')
+  const data = _data
+  data.value = data.value || 0
 
   return {
     getValue() {
-      return value
+      return data.value
     },
 
     increase() {
-      value++
+      data.value++
     }
   }
 }
